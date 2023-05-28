c83658909bac341c9affaa18590b531db88d0f03
AnatolKzh <bolshoymedvedtoliano@gmail.com>
2023-05-28 23:43:28 +0200

    create

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,112 @@
-import Image from 'next/image'
+"use client";
+import Image from "next/image";
+
+import { Configuration, OpenAIApi } from "openai";
+import { ChangeEvent, useEffect, useState } from "react";
+
+const configuration = new Configuration({
+  apiKey: process.env.NEXT_PUBLIC_CHAT_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
 
 export default function Home() {
+  const [twit, setTwit] = useState("");
+  const [hashTagResult, setHashTagResult] = useState("");
+  const [numberRange, setNumberRange] = useState("3");
+
+  const onGenerate = () => {
+    const completion = openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: generatePrompt(twit, numberRange),
+      temperature: 0.45,
+      max_tokens: 500,
+    });
+    completion.then((res) => {
+      const text = res.data.choices[0].text;
+      console.log(res);
+      setHashTagResult(text ? text : "");
+    });
+  };
+
+  const onPostFieldChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    setTwit(event.target.value);
+  };
+
+  const getResult = () => {
+    return hashTagResult;
+  };
+
+  const handleOnChangeHashTagRange = (event: ChangeEvent<HTMLInputElement>) => {
+    setNumberRange(event.target.value);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="flex items-center justify-center h-screen">
+      <div className="form min-w-[50%]">
+        <label
+          htmlFor="message"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Input Your Twitter Post
+        </label>
+        <textarea
+          id="message"
+          className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 
+          rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500
+           dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400
+            dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500
+            h-60"
+          placeholder="Write your post here..."
+          onChange={onPostFieldChange}
+        ></textarea>
+
+        <div className="flex items-center justify-center p-10">
+          <label
+            htmlFor="default-range"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            How much hastags?
+          </label>
+          <input
+            id="default-range"
+            type="range"
+            min={1}
+            max={10}
+            defaultValue={3}
+            className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+            onChange={handleOnChangeHashTagRange}
+          />
+          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white pl-10">
+            {numberRange}
+          </label>
+        </div>
+
+        <div className="flex items-center justify-center p-10">
+          <button
+            type="submit"
+            className="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800"
+            onClick={() => onGenerate()}
+          >
+            Generate Perfect Tags
+          </button>
+        </div>
+
+        <div className="flex items-center justify-center p-10">
+          {getResult()}
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
+}
+
+function generatePrompt(twit: string, max: string) {
+  return ` 
+  1. Generate top twitter hashtags for the text below;
+  2. Needed format is: #one, #two and etc.;
+  3. Hash tags should not be empty;
+  4. Numbers of hashtags: ${max};
+
+  Twit: ${twit}
+  `;
 }
